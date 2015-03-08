79b5236f560816d67ad211ec041eb5997db74b96
joshkirstein <joshkirstein@aol.com>
2015-03-08 15:27:59 -0400

    i hate committing

---
+++ src/org/escaperun/game/model/stage/areaeffect/AreaEffect.java
@@ -0,0 +1,25 @@
+package org.escaperun.game.model.stage.areaeffect;
+
+import org.escaperun.game.model.Touchable;
+import org.escaperun.game.model.entities.Entity;
+import org.escaperun.game.view.Decal;
+import org.escaperun.game.view.Renderable;
+
+public abstract class AreaEffect implements Renderable, Touchable {
+    private Decal decal;
+
+    public AreaEffect(Decal decal) {
+        this.decal = decal;
+    }
+
+    @Override
+    public Decal[][] getRenderable() {
+        return new Decal[][] {{ this.decal }};
+    }
+
+    @Override
+    public abstract void onTouch(Entity e);
+
+    @Override
+    public abstract void doAction(Entity e);
+}
--- src/org/escaperun/game/model/stage/areaeffect/TeleportationAreaEffect.java
+++ src/org/escaperun/game/model/stage/areaeffect/TeleportationAreaEffect.java
@@ -1,4 +1,4 @@
-package org.escaperun.game.model.stage.tile.areaeffect;
+package org.escaperun.game.model.stage.areaeffect;
 
 import org.escaperun.game.model.Position;
 import org.escaperun.game.model.entities.Entity;
