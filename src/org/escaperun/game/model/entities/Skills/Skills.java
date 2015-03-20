68f8fe1f85d9e5981293dd1cfd9f64c4e30d9c86
Jeff G <drak@ufl.edu>
2015-03-19 20:19:48 -0400

    Had to manually add in the deletions.

--- src/org/escaperun/game/model/entities/Skills/Skills.java
+++
@@ -1,9 +0,0 @@
-package org.escaperun.game.model.entities.Skills;
-
-/**
- * Created by abcronin on 3/15/2015.
- */
-public abstract class Skills {
-    private String beneficialStat;
-    public abstract String getBeneficialStat();
-}
