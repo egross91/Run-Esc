f5d08f841d5403c0216b99894268a6ee5544e31c
aemery <agemery2@gmail.com>
2015-03-20 22:16:07 -0400

    working on OffensiveRating. Need a way to alert it when the equipped weapon changes

--- src/org/escaperun/game/model/entities/statistics/OffensiveRating.java
+++ src/org/escaperun/game/model/entities/statistics/OffensiveRating.java
@@ -1,22 +1,33 @@
 package org.escaperun.game.model.entities.statistics;
 
 public class OffensiveRating extends DerivedStatistic<Integer>{
-    public OffensiveRating(Strength strength, Level level) {
 
+    Strength str;
+    Level lvl;
+    int weaponDamage;
+    int offensiveRating;
+
+    //need a way to alert when weapon changes
+    public OffensiveRating(Strength strength, Level level, int equippedWeaponDamage) {
+        str = strength;
+        lvl = level;
+        weaponDamage = equippedWeaponDamage;
+        recalculate();
     }
 
-    @Override
-    public void notifyChange() {
-
+    protected void recalculate() {
+        //idk I made something up
+        //also need a way to notify when weapon changes
+        this.offensiveRating = (str.getCurrent() * lvl.getCurrent()) + weaponDamage;
     }
 
     @Override
     public Integer getBase() {
-        return null;
+        return offensiveRating;
     }
 
     @Override
     public Integer getCurrent() {
-        return null;
+        return offensiveRating;
     }
 }
