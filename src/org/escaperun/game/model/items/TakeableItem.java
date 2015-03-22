0d2a44e5f53529d1858b67ebbb533963b42ab1ee
Eric <eric.bgross@gmail.com>
2015-03-21 21:20:50 -0400

    [EquipableItem] Added Statistics to TakeableItem for Aleks

--- src/org/escaperun/game/model/items/TakeableItem.java
+++ src/org/escaperun/game/model/items/TakeableItem.java
@@ -1,17 +1,28 @@
 package org.escaperun.game.model.items;
 
 import org.escaperun.game.model.entities.Entity;
+import org.escaperun.game.model.entities.statistics.StatisticContainer;
 import org.escaperun.game.view.Decal;
 
 public abstract class TakeableItem extends TouchableItem {
+    private StatisticContainer statistics;
 
     public TakeableItem(Decal decal) {
         super(decal);
     }
 
+    public TakeableItem(Decal decal, StatisticContainer stats) {
+        super(decal);
+        this.statistics = stats;
+    }
+
     public abstract String getName();
 
     public abstract String getDescription();
+
+    public StatisticContainer getStatistics() {
+        return this.statistics;
+    }
 
     @Override
     public boolean isCollidable() {
