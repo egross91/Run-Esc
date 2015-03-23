20d1df23da0ed9ae26c5b41af25216cced0867c4
joshkirstein <joshkirstein@aol.com>
2015-03-22 22:57:03 -0400

    Flames now

--- src/org/escaperun/game/model/items/equipment/weapons/WeaponItem.java
+++ src/org/escaperun/game/model/items/equipment/weapons/WeaponItem.java
@@ -6,6 +6,7 @@ import org.escaperun.game.model.items.equipment.visitors.WeaponElement;
 import org.escaperun.game.view.Decal;
 
 public abstract class WeaponItem extends EquipableItem implements WeaponElement {
+
     public WeaponItem(Decal decal) {
         super(decal, EquipmentSlot.WEAPON);
     }
